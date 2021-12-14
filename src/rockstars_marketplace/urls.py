4358cc45500f200fc981d95f1bd5c09f1d7a257a
Samuel Gonzalez <xame2005@gmail.com>
2021-12-13 19:06:10 -0600

    [CREATED] Project Setup

---
+++ src/rockstars_marketplace/urls.py
@@ -0,0 +1,18 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.Homeview.as_view(), name='home'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
